633fbcb72fb0d961f7cf7001b00f6264f8b6c9b1
junaid014 <165922318+junaid014@users.noreply.github.com>
2024-11-03 23:08:03 +0530

    Delete NameDecending.java
    problems overcome for previous code:
    previous code:
    1.the Comparator and comparable is used to compare two strings.
    2.two classes are use to compare one for increasing order another for decreasing order 
    new code:
    1.by using lambda expression we can  two strings in 2 lines but by using streams we can compare two strings in single line.
    2.only one class is used to compare for both orders

--- NameDecending.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-public class NameDecending implements Comparator<Employee> {
-    @Override
-    public int compare(Employee o1, Employee o2) {
-        String e1=o1.name;
-        String e2=o2.name;
-        return e2.compareTo(e1);
-    }
-}
